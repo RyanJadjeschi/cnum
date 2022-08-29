1818e76806aa9f399c7fde49f8471e23f3540ce8
SP4RT4NxVegeta <111003422+RyanJadjeschi@users.noreply.github.com>
2022-08-29 18:39:51 +0000

    .

---
+++ 4/main.jl
@@ -0,0 +1,18 @@
+using .MathConstants:e
+
+f(x) = e^x-x-2
+
+error = 10^-3
+
+  function fixedpoint(a, g)
+  x=g(a)
+  while abs(x-a) > error
+    a =x
+    x = g(a)
+  end
+  return x
+end
+
+g(x) = log(x+2)
+r = fixedpoint(1, g)
+println(r)
